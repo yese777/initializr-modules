a2a26b19b2b1897868d909fd665c4abee6988e3b
zhangqingfu <15195988693@163.com>
2020-08-31 15:30:59 +0800

    更新

--- initializr-system/src/main/java/com/yese/controller/TestController.java
+++ initializr-system/src/main/java/com/yese/controller/TestController.java
@@ -8,10 +8,15 @@ import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.constraints.NotNull;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Api(tags = "测试")
@@ -32,5 +37,21 @@ public class TestController {
         int a = 1 / 0;
     }
 
+    @PostMapping("/set")
+    public void set(HttpSession session, @RequestParam("key") String key, @RequestParam("value") String value) {
+        session.setAttribute(key, value);
+    }
 
+    @GetMapping("/getAll")
+    public Map<String, Object> getAll(HttpSession session) {
+        Map<String, Object> result = new HashMap<>();
+        // 遍历
+        for (Enumeration<String> enumeration = session.getAttributeNames(); enumeration.hasMoreElements(); ) {
+            String key = enumeration.nextElement();
+            Object value = session.getAttribute(key);
+            result.put(key, value);
+        }
+        // 返回
+        return result;
+    }
 }
