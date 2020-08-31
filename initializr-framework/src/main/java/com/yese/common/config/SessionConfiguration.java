94ffebbbc824e9a2e9dfda5bc4f80110a024078e
zhangqingfu <15195988693@163.com>
2020-08-31 16:58:25 +0800

    更新

--- initializr-framework/src/main/java/com/yese/common/config/SessionConfiguration.java
+++ initializr-framework/src/main/java/com/yese/common/config/SessionConfiguration.java
@@ -7,8 +7,16 @@ import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 
+/**
+ * 开启自动化配置 Spring Session 使用 Redis 作为数据源。该注解有如下属性：
+ *
+ * maxInactiveIntervalInSeconds:设置 Session 的失效时间，单位为秒。默认1800秒。
+ * redisNamespace:为键定义统一的命名前缀。默认:spring:session
+ * redisFlushMode:Redis 会话刷新模式(RedisFlushMode)。目前有两种，默认为 RedisFlushMode.ON_SAVE 。
+ * cleanupCron:清理 Redis Session 会话过期的任务执行 CRON 表达式，默认为 "0 * * * * *" 每分钟执行一次。虽然说，Redis 自带了 key 的过期，但是惰性删除策略，实际过期的 Session 还在 Redis 中占用内存。所以，Spring Session 通过定时任务，删除 Redis 中过期的 Session ，尽快释放 Redis 的内存。
+ */
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
 @Configuration
-@EnableRedisHttpSession
 public class SessionConfiguration {
 
     /**
