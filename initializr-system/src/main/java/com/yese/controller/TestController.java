126a86cd3033497d31d57400ce218401a1b31d8c
zhangqingfu <15195988693@163.com>
2020-09-01 00:24:51 +0800

    更新

--- initializr-system/src/main/java/com/yese/controller/TestController.java
+++ initializr-system/src/main/java/com/yese/controller/TestController.java
@@ -3,8 +3,8 @@ package com.yese.controller;
 
 import com.baomidou.mybatisplus.extension.api.R;
 import com.yese.common.annotation.Log;
-import com.yese.common.enums.BusinessType;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,26 +24,32 @@ import java.util.Map;
 @RestController
 @Validated
 public class TestController {
-
+    @ApiOperation("测试参数校验")
     @Log(module = "测试模块", content = "测试参数校验")
     @GetMapping("validation")
     public R<String> validation(@RequestParam("a") @NotNull(message = "{id.NotNull}") String a) {
         return R.ok(a);
     }
 
-    @Log(module = "测试模块", content = "测试异常", businessType = BusinessType.OTHER)
+    @ApiOperation("测试异常")
+    @Log(module = "测试模块", content = "测试异常")
     @GetMapping("exception")
     public void exception() {
         int a = 1 / 0;
     }
 
-    @PostMapping("/set")
-    public void set(HttpSession session, @RequestParam("key") String key, @RequestParam("value") String value) {
+    @ApiOperation("测试session设置值")
+    @Log(module = "测试模块", content = "测试session设置值")
+    @PostMapping("/session")
+    public R session(HttpSession session, @RequestParam("key") String key, @RequestParam("value") String value) {
         session.setAttribute(key, value);
+        return R.ok(null);
     }
 
-    @GetMapping("/getAll")
-    public Map<String, Object> getAll(HttpSession session) {
+    @ApiOperation("测试获取session中的值")
+    @Log(module = "测试模块", content = "测试获取session中的值")
+    @GetMapping("/session/All")
+    public R getAll(HttpSession session) {
         Map<String, Object> result = new HashMap<>();
         // 遍历
         for (Enumeration<String> enumeration = session.getAttributeNames(); enumeration.hasMoreElements(); ) {
@@ -52,6 +58,6 @@ public class TestController {
             result.put(key, value);
         }
         // 返回
-        return result;
+        return R.ok(result);
     }
 }
